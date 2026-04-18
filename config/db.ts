2c3c285f4cd2820183d1f4708706dd114974665b
Achraf Saidi <achraf-saidi@outlook.com>
2026-04-18 04:10:52 +0100

    first commit & basic backend server config

---
+++ config/db.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const connectDB = async () => {
+    mongoose.connection.on("connected", () => {
+        console.log("Connected to MongoDB");
+    });
+
+    mongoose.connection.on("error", (err) => {
+        console.error("Error connecting to MongoDB:", err);
+    });
+    mongoose.connection.on("disconnected", () => {
+        console.log("Disconnected from MongoDB");
+    });
+
+    try {
+        await mongoose.connect(process.env.DB_URI as string);
+    } catch (error) {
+        console.error("Failed to connect to MongoDB:", error);
+    }   
+}
+export default connectDB;
