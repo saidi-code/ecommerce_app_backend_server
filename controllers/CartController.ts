1810c991defde992f69112d06a3915b579bac1fa
Achraf Saidi <achraf-saidi@outlook.com>
2026-04-25 15:27:18 +0100

    adapte same controller for frontend integration

--- controllers/CartController.ts
+++ controllers/CartController.ts
@@ -1,125 +1,163 @@
-import {Request,Response} from "express"
-import Cart from "../models/Cart.js"
-import Product from "../models/Product.js"
-// Get User Cart 
+import { Request, Response } from "express";
+import Cart from "../models/Cart.js";
+import Product from "../models/Product.js";
+// Get User Cart
 // Get /api/v1/cart
-export const getCart = async (req:Request,res:Response)=>{
-   try{
+export const getCart = async (req: Request, res: Response) => {
+  try {
     let cart = await Cart.findOne({
-        user:req.user._id
-    }).populate("items.product","name images price stock")
-    if(!cart){
-        cart = await Cart.create({user:req.user._id,items:[]})
+      user: req.user._id,
+    }).populate("items.product", "name images price stock");
+    if (!cart) {
+      cart = await Cart.create({ user: req.user._id, items: [] });
     }
-    console.log("Cart found for user", req.user._id, cart);
-    res.json({"success":true,data:cart})
-   }catch(error:any){
-    res.status(500).json({"success":false,"message":error.message})
-   }
-} 
+    res.json({ success: true, data: cart });
+  } catch (error: any) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
 // Add To Cart
 // POST /api/v1/cart/add
-export const addToCart = async (req:Request,res:Response)=>{
-   
-    try {
-        const {productId,quantity=1,size} = req.body
-        const product = await Product.findById(productId)
-        if(!product){
-            return res.status(404).json({success:false,message:"Product not found"})
-        }
-        if(product.stock< quantity){
- return res.status(400).json({success:false,message:"Insufficent stock"})
-        }
-        let cart = await Cart.findOne({user:req.user._id})
-        if(!cart){
-            cart = new Cart ({user:req.user._id, items:[]})
-        }
-        const existingItem = cart.items.find((item)=>{
-            return item.product.toString()=== productId && item.size === size
-        })
-        if(existingItem){
-            existingItem.quantity += quantity;
-            existingItem.price = product.price
-        }else{
-            cart.items.push({
-                product:productId,
-                quantity,
-                price:product.price,
-                size
-            })
-        }
-        cart.calculateTotal()
-        await cart.save()
-        await cart.populate("items.product","name images price stock")
-        res.json({"success":true,data: cart})
-    } catch (error:any) {
-          res.status(500).json({"success":false,"message":error.message})
+export const addToCart = async (req: Request, res: Response) => {
+  try {
+    const { productId, quantity = 1, size } = req.body;
+    const product = await Product.findById(productId);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Product not found" });
     }
-} 
+    if (product.stock < quantity) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Insufficent stock" });
+    }
+    let cart = await Cart.findOne({ user: req.user._id });
+    if (!cart) {
+      cart = new Cart({ user: req.user._id, items: [] });
+    }
+    const existingItem = cart.items.find((item) => {
+      return item.product.toString() === productId && item.size === size;
+    });
+    if (existingItem) {
+      existingItem.quantity += quantity;
+      existingItem.price = product.price;
+    } else {
+      cart.items.push({
+        product: productId,
+        quantity,
+        price: product.price,
+        size,
+      });
+    }
+    cart.calculateTotal();
+    await cart.save();
+    await cart.populate("items.product", "name images price stock");
+    res.json({ success: true, data: cart });
+  } catch (error: any) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
 // Update Item Quantity
 // PUT /api/v1/cart/item/:productId
-export const updateCartItem = async (req:Request,res:Response)=>{
-    try {
-        const {quantity,size} = req.body
-        const {productId}=req.params
-        const cart = await Cart.findOne({user:req.user._id})
-        if(!cart){
-            return res.status(404).json({"success":false,message:"Cart not found"})
-        }
-        const item = cart.items.find((item)=>item.product.toString()=== productId && item.size === size)
-        if(!item){
-            return  res.status(404).json({"success":false,message:"Item not in cart"})
-        }
-        if(quantity<= 0){
-            cart.items = cart.items.filter((item)=>item.product.toString()!= productId)
-        }else {
-            const product = await Product.findById(productId)
-            if(product!.stock<quantity){
-                 return res.status(400).json({success:false,message:"Insufficent stock"})
-            }
-            item.quantity = quantity;
-
-        }
-        cart.calculateTotal()
-        await cart.save()
-        await cart.populate("items.product","name images price stock")
-        res.json({"success":true,"data":cart})
-
-    } catch (error:any) {
-        res.status(500).json({"success":false,"message":error.message})
+export const updateCartItem = async (req: Request, res: Response) => {
+  try {
+    const { quantity, size } = req.body;
+    const { productId } = req.params;
+    console.log(productId, quantity, size);
+    const cart = await Cart.findOne({ user: req.user._id });
+    if (!cart) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Cart not found" });
     }
-} 
-// Delete Cart Item 
+    //find item in cart
+
+    const item = cart.items.find(
+      (item) =>
+        item.product.toString() === productId.toString() && item.size === size,
+    );
+    console.log("cart items:", cart.items);
+    if (!item) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Item not in cart" });
+    }
+    console.log(
+      "selecting item with productId:",
+      productId,
+      "and size:",
+      size,
+      "Found item:",
+      item,
+    );
+    if (quantity <= 0) {
+      cart.items = cart.items.filter(
+        (item) =>
+          item.product.toString() !== productId.toString() ||
+          item.size !== size,
+      );
+    } else {
+      const product = await Product.findById(productId);
+      if (product!.stock < quantity) {
+        return res
+          .status(400)
+          .json({ success: false, message: "Insufficent stock" });
+      }
+      item.quantity = quantity;
+    }
+    cart.calculateTotal();
+    await cart.save();
+    await cart.populate("items.product", "name images price stock");
+    res.json({ success: true, data: cart });
+  } catch (error: any) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+// Delete Cart Item
 // DELETE api/v1/cart/item/:productId
-export const deleteCartItem = async (req:Request,res:Response)=>{
-    try {
-        const {size} = req.query;
-        const cart = await Cart.findOne({user:req.user._id})
-        if(!cart || !size){
-            return res.status(404).json({"success":false,"message":"Cart not found"})
-        }
-        cart.items = cart.items.filter((item)=>item.product.toString()!== req.params.productId || item.size !== size)
-        cart.calculateTotal()
-        await cart.save()
-        await cart.populate("items.product","name images price stock")
-        res.json({"success":true,data:cart})
-    } catch (error:any) {
-        res.status(500).json({"success":false,"message":error.message})
+export const deleteCartItem = async (req: Request, res: Response) => {
+  try {
+    const size = req.query.size as string | undefined;
+    const cart = await Cart.findOne({ user: req.user._id });
+    if (!cart) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Cart not found" });
     }
-} 
-// Clear Cart  
+    if (!size) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Size query parameter is required" });
+    }
+    cart.items = cart.items.filter(
+      (item) =>
+        item.product.toString() !== req.params.productId || item.size !== size,
+    );
+    cart.calculateTotal();
+    await cart.save();
+    await cart.populate("items.product", "name images price stock");
+    console.log("cart data", cart);
+    res.json({ success: true, data: cart });
+  } catch (error: any) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+// Clear Cart
 // DELETE api/v1/cart
-export const clearCart = async (req:Request,res:Response)=>{
-    try {
-        const cart = await Cart.findOne({user:req.user._id})
-        if(!cart){
-             return res.status(404).json({"success":false,"message":"Cart not found"})
-        }
-        cart.items = [],
-        cart.totalAmount = 0
-        await cart.save()
-        
-    } catch (error:any) {
-          res.status(500).json({"success":false,"message":error.message})
+export const clearCart = async (req: Request, res: Response) => {
+  try {
+    const cart = await Cart.findOne({ user: req.user._id });
+    if (!cart) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Cart not found" });
     }
-} 
+    cart.items = [];
+    cart.totalAmount = 0;
+    await cart.save();
+    res.json({ success: true, message: "Cart cleared", data: cart });
+  } catch (error: any) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
--- routes/OrderRoutes.ts
+++ routes/OrderRoutes.ts
@@ -1,11 +1,17 @@
-import express from "express"
-import {protect,authorize} from "../middlewares/auth.js"
-import {getUserOrders,getOrder,createOrder,updateOrderStatus,getAllOrdes} from "../controllers/OrderController.js"
-const router = express.Router()
+import express from "express";
+import {
+    createOrder,
+    getAllOrdes,
+    getOrder,
+    getUserOrders,
+    updateOrderStatus,
+} from "../controllers/OrderController.js";
+import { authorize, protect } from "../middlewares/auth.js";
+const router = express.Router();
 
-router.get("/",protect,getUserOrders)
-router.get("/:id",protect,getOrder)
-router.post("/create",protect,createOrder)
-router.put("/:id/status",protect,authorize("admin"),updateOrderStatus)
-router.get("/admin/all",protect,authorize("admin"),getAllOrdes)
-export default router
+router.get("/", protect, getUserOrders);
+router.get("/:id", protect, getOrder);
+router.post("/", protect, createOrder);
+router.put("/:id/status", protect, authorize("admin"), updateOrderStatus);
+router.get("/admin/all", protect, authorize("admin"), getAllOrdes);
+export default router;
